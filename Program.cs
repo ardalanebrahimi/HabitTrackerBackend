2489e58b1dcaf9b3e632aeb5c936244fa9044f5f
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2025-02-09 19:19:08 +0100

    fix: enable swagger on prod

--- Program.cs
+++ Program.cs
@@ -53,12 +53,8 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build(); // Call AFTER service registrations
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
